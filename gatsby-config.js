1be0a10f11af6a1abe2701c51232b8ceeba470c6
Lokolin <lokolin777@mail.ru>
2019-11-29 10:46:59 +0300

    register custom cms.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,6 +93,11 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-lodash`,
     `gatsby-plugin-sitemap`,
-    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
   ],
 }
